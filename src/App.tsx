adc23366dc89cdbb7da84b4ae47378499d8d4384
Søren Krogh <soren1@hotmail.com>
2019-09-20 17:12:07 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const [data, setData] = useState("Hest");
+
+  function buttonClick() {
+    fetch('http://localhost:8080/accountvalue/search?to=1553310001000')
+        .then(res => res.json())
+        .then((result) => {
+          const items = result.map((item: any, key: any) => <div key={item.id}>{item.accountValue}</div>);
+          console.log(JSON.stringify(result));
+          setData(items);
+        })
+        .catch(console.log);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {data}
+        </div>
+        <Button onClick={buttonClick} />
       </header>
     </div>
   );
